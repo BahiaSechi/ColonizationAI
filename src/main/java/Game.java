50d2b2f602bc73b3699446c6c2a342afe9b683d8
BahiaSechi <mdmushrm@gmail.com>
2021-02-12 12:01:04 +0100

    work on displaying

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,19 @@
+import planet.Planet;
+
 public class Game {
 
+    private Planet planet;
 
+    public Game(Planet planet) {
+        this.planet = planet;
+    }
 
+    public Planet getPlanet() {
+        return planet;
+    }
+
+    public void setPlanet(Planet planet) {
+        this.planet = planet;
+    }
 }
+
--- src/main/java/view/PlanetController.java
+++ src/main/java/view/PlanetController.java
@@ -4,19 +4,44 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class PlanetController extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/PlanetView.fxml"));
-        stage.setTitle("Colonisation");
-        stage.setScene(new Scene(root, 1360, 750));
-        stage.setResizable(false);
-        stage.show();
-    }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setTitle("Colonisation");
+        stage.setResizable(false);
+
+        GridPane gridpane = new GridPane();
+
+        Image img = new Image("/sprites/world/grasstest.png");
+
+        for(int i = 0 ; i < 21 ; i++) {
+            for (int j = 0; j < 21; j++) {
+                ImageView imgView = new ImageView();
+                imgView.setFitHeight(20);
+                imgView.setFitWidth(20);
+                imgView.setImage(img);
+                gridpane.setConstraints(imgView, i, j);
+                gridpane.getChildren().addAll(imgView);
+            }
+        }
+
+        // width / height
+        stage.setScene(new Scene(gridpane, gridpane.getVgap(), gridpane.getHgap()));
+
+        stage.show();
+
     }
 }
