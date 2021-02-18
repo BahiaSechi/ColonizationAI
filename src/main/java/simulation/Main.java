38783307e242884c9e1724ae34e9fac1e9e81a92
Bastien Prunier <bprunier@ecole.ensicaen.fr>
2021-02-18 11:44:08 +0100

    Useless main

--- src/main/java/simulation/Main.java
+++ src/main/java/simulation/Main.java
@@ -6,11 +6,5 @@ import simulation.planet.tiles.TileType;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello !");
-
-        // Tests tiles
-        Tile tile = new Tile(5,8,7,5, TileType.WATER, null, null);
-        System.out.println(tile.getTileX());
-        System.out.println(tile.getType());
-
     }
 }
