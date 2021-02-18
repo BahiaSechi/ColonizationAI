b6adf7b52dbc66695efdc030f7071bfc9dcd4f60
BahiaSechi <mdmushrm@gmail.com>
2021-02-18 09:40:40 +0100

    Maven fuzzy  logic

--- src/main/java/view/PlanetController.java
+++ src/main/java/view/PlanetController.java
@@ -1,6 +1,9 @@
 package view;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -21,20 +24,21 @@ public class PlanetController extends Application {
 
         stage.setTitle("Colonisation");
         stage.setResizable(false);
+        stage.getIcons().add(new Image("/sprites/robot/icon.jpg"));
 
         GridPane gridpane = new GridPane();
         Game game = new Game();
         Planet planet = game.getPlanet();
         Tile[][] tile = planet.getMap();
 
-        for(int i = 0 ; i < planet.getSIZE_Y() ; i++) {
+        for (int i = 0; i < planet.getSIZE_Y(); i++) {
             for (int j = 0; j < planet.getSIZE_X(); j++) {
 
                 Image img = new Image(tile[j][i].getType().getNameFile());
 
                 ImageView imgView = new ImageView();
-                imgView.setFitHeight(20);
-                imgView.setFitWidth(20);
+                imgView.setFitHeight(24);
+                imgView.setFitWidth(24);
                 imgView.setImage(img);
                 GridPane.setConstraints(imgView, i, j);
                 gridpane.getChildren().addAll(imgView);
@@ -42,8 +46,17 @@ public class PlanetController extends Application {
         }
 
         // width / height
-        stage.setScene(new Scene(gridpane, gridpane.getVgap(), gridpane.getHgap()));
+        stage.setScene(new Scene(gridpane, 24 * planet.getSIZE_X(), 24 * planet.getSIZE_Y()));
 
         stage.show();
+    }
+
+    // Close properly the GUI.
+    public static class ExitButtonListener implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent arg0) {
+            Platform.exit();
+        }
     }
 }
