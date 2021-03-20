79671afa28fd02c3962870cea0b4923ea919b34d
Bastien Prunier <bprunier@ecole.ensicaen.fr>
2021-03-20 11:08:44 +0100

    Cleaning of the master

--- src/main/java/simulation/robots/moves/Immobile.java
+++ src/main/java/simulation/robots/moves/Immobile.java
@@ -2,11 +2,13 @@ package simulation.robots.moves;
 
 import simulation.robots.Pos;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Immobile extends MoveStrategy {
     @Override
     public List<Pos> findNewPath() {
-        return new List.of(null);
+        //return new List.of(null);
+        return new LinkedList<>();
     }
 }
