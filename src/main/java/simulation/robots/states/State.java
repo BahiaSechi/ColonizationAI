139d0ec8110faeb82514ceba3275d537a60307c2
RABOTIN Matéo <mateo@rabotin.net>
2021-03-21 15:10:08 +0100

    Synchronized State pos

--- src/main/java/simulation/robots/states/State.java
+++ src/main/java/simulation/robots/states/State.java
@@ -3,14 +3,15 @@ package simulation.robots.states;
 import javafx.util.Pair;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Synchronized;
 import simulation.robots.Pos;
 import simulation.robots.Robot;
 
 import java.util.Optional;
 
 public abstract class State {
-    @Getter
-    @Setter
+    @Getter(onMethod_ = {@Synchronized})
+    @Setter(onMethod_ = {@Synchronized})
     private Pos pos;
 
     public State(Pos pos) {
