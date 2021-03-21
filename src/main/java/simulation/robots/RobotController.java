4ba54f59488254bccbb9936bd6b236f1c7aa3fc0
RABOTIN Matéo <mateo@rabotin.net>
2021-03-21 19:54:40 +0100

    Finished newDay

--- src/main/java/simulation/robots/RobotController.java
+++ src/main/java/simulation/robots/RobotController.java
@@ -46,5 +46,8 @@ public class RobotController {
      * Run the day for robots, move each robot on his daily tasks.
      */
     public void runDay() {
+        for (Robot robot : robots.values()) {
+            robot.nextMove();
+        }
     }
 }
