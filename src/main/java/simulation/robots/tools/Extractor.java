cf131dc6494e4a3c8c24939353201fe7a47379b4
RABOTIN Mateo <mateo.rabotin@ecole.ensicaen.fr>
2021-03-21 18:42:00 +0100

    Revert "Merge branch 'feature/colony_score' into 'master'"
    This reverts merge request !10

--- src/main/java/simulation/robots/tools/Extractor.java
+++ src/main/java/simulation/robots/tools/Extractor.java
@@ -3,7 +3,6 @@ package simulation.robots.tools;
 
 import simulation.planet.tiles.Tile;
 import simulation.planet.tiles.TileType;
-import simulation.robots.OperatorRobot;
 import simulation.robots.Pos;
 import simulation.robots.Robot;
 
@@ -12,12 +11,8 @@ public class Extractor extends Tool {
     public void use(Robot robot) {
         Pos pos = robot.getState().getPos();
         Tile tile = planet.getTile(pos.getX(), pos.getY());
-
         if (tile.getType() == TileType.ORE) {
-            OperatorRobot operator = robot.getController().getOperator();
-            int exploit_amount = 10;
-            planet.exploit(pos, exploit_amount);
-            operator.setOreStock(operator.getOreStock() + exploit_amount);
+            planet.exploit(pos, 1);
         }
     }
 }
