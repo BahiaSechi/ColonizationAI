56a0ee2be23cfa49f91873b1bc56abbf8c2e3a1e
RABOTIN Matéo <mateo@rabotin.net>
2021-03-21 15:18:46 +0100

    Added lombok to robot controller

--- src/main/java/simulation/robots/RobotController.java
+++ src/main/java/simulation/robots/RobotController.java
@@ -1,17 +1,26 @@
 package simulation.robots;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class RobotController {
+    @Getter
+    @Setter
     private Pos                 colonyCenter;
+    @Getter
+    @Setter
     private OperatorRobot       operator;
+    @Getter
+    @Setter
     private Map<Integer, Robot> robots;
 
     public RobotController(Pos colonyCenter) {
         this.colonyCenter = colonyCenter;
         this.operator = new OperatorRobot(this, 0);
-        this.robots = new HashMap<Integer, Robot>();
+        this.robots = new HashMap<>();
     }
 
     public boolean addRobot(int id, Robot robot) {
@@ -31,33 +40,5 @@ public class RobotController {
     public Pos getAbsolutePos(Robot robot) {
         return new Pos(colonyCenter.getX() + robot.getState().getPos().getX(),
                 colonyCenter.getY() + robot.getState().getPos().getY());
-    }
-
-    public int getTileNumber(Pos pos) {
-        return pos.getY() * 23 + pos.getX();
-    }
-
-    public Pos getColonyCenter() {
-        return colonyCenter;
-    }
-
-    public void setColonyCenter(Pos colonyCenter) {
-        this.colonyCenter = colonyCenter;
-    }
-
-    public OperatorRobot getOperator() {
-        return operator;
-    }
-
-    public void setOperator(OperatorRobot operator) {
-        this.operator = operator;
-    }
-
-    public Map<Integer, Robot> getRobots() {
-        return robots;
-    }
-
-    public void setRobots(Map<Integer, Robot> robots) {
-        this.robots = robots;
     }
 }
